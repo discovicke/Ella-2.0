60526a8e53cd4cf87aa6f26c928b503742571aad
Marcus Löf <LeafMaster1@outlook.com>
2026-02-02 14:42:14 +0100

    Added task for IRoomRepo

--- Backend/app/Core/Interfaces/IRoomRepository.cs
+++ Backend/app/Core/Interfaces/IRoomRepository.cs
@@ -5,4 +5,12 @@ namespace Backend.app.Core.Interfaces;
 public interface IRoomRepository
 {
     // Define methods for room repository here
+    Task GetRoomsAsync();
+    Task GetRoomByIdAsync(int id);
+    Task CreateRoomAsync();
+    Task UpdateRoomAsync();
+    Task DeleteRoomAsync();
+    Task SaveChangesAsync();
+    
+     
 }
