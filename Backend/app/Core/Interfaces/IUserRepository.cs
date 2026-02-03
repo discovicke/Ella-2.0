e983d4f87f8ce16916556315d900d83186e032b6
aponten <andyponten@gmail.com>
2026-02-03 13:32:20 +0100

    update: user query methods in IUserRepository

--- Backend/app/Core/Interfaces/IUserRepository.cs
+++ Backend/app/Core/Interfaces/IUserRepository.cs
@@ -13,9 +13,18 @@ public interface IUserRepository
     
     Task<IEnumerable<User>> GetAllUsersAsync();
     Task<User?> GetUserByIdAsync(int id);
-    Task<IEnumerable<User>> GetUsersByClassAsync(string classname);
-    Task<IEnumerable<User>> GetUsersByRoleAsync(UserRole role);
-    Task<User?> CreateUserAsync();
+    Task<User?> FindUserByEmailAsync(string email);
+    Task<User?> FindUserByClassAsync(int classId);
+    Task<User?> FindUserByRoleAsync(UserRole role);
+    Task<User?> FindUserByNameAsync(string name);
+    Task<User?> CreateUserAsync(User user);
     Task<User?> UpdateUserAsync(int id, User user);
     Task<User?> DeleteUserAsync(int id);
+
+    // New Tasks
+    Task<IEnumerable<User>> GetUsersByClassAsync(string classname);
+    Task<IEnumerable<User>> GetUsersByRoleAsync(UserRole role);
+    
+
+    
 }
