5c1959ccbcfd0c609fba601e260f77afbc0db883
aponten <andyponten@gmail.com>
2026-02-04 14:42:27 +0100

    feat(users): define User DTOs (Create, Update, Response)

--- Backend/app/Core/Models/DTO/UserDto.cs
+++ Backend/app/Core/Models/DTO/UserDto.cs
@@ -1,8 +1,39 @@
+﻿using Backend.app.Core.Enums;
+
 namespace Backend.app.Core.DTO;
 
-public class UserDto
-{
+
+
     // Data Transfer Objects for User
     // TODO: Define CreateUserDto, UserResponseDto, UpdateUserDto
     // Reference: src/modules/users/user.dto.js
-}
+public record UserResponseDto(
+    int id,
+    string email ,
+    string? DisplayName ,
+    UserRole Role ,
+    string? UserClass
+);
+
+public record CreateUserDto(
+    int Id,
+    string Email,
+    string? DisplayName,
+    UserRole Role,
+    string Password,
+    string? UserClass
+);
+
+public record UpdateUserDto(
+    int Id,
+    string Email,
+    string? DisplayName,
+    UserRole Role,
+    string Password,
+    string? UserClass,
+    BannedStatus IsBanned
+);
+
+public record DeleteUserDto(
+    int Id
+);
