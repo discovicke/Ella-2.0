d0d8f83e28faa64567635af1e446394e4fa65a41
Marcus Löf <LeafMaster1@outlook.com>
2026-02-04 11:27:38 +0100

    Add bookings route group and GET endpoint

--- Backend/app/API/Endpoints/BookingEndpoints.cs
+++ Backend/app/API/Endpoints/BookingEndpoints.cs
@@ -1,7 +1,28 @@
+using Backend.app.Core.Enums;
+using Backend.app.Core.Models.DTO;
+using Backend.app.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Backend.app.API.Endpoints;
 
-public class BookingEndpoints
+public static class BookingEndpoints
 {
     // TODO: Migrate all booking endpoints
     // Reference: src/modules/bookings/booking.routes.js + booking.controller.js
+    
+    public static RouteGroupBuilder MapBookingEndPoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/bookings").WithTags("Bookings");
+
+        
+         group.MapGet("/",
+          async (BookingService service) =>
+          {
+              var bookings = await service.GetAllBookingsAsync();
+              return Results.Ok(bookings);
+          });
+
+        return group;
+    }
+    
 }
