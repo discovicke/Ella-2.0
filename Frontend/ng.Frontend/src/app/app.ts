989a015089aa9a399d57e1ab24adcb9892746d33
Viktor Johansson <discovicke@gmail.com>
2026-02-10 13:12:49 +0100

    feat(app): integrate modal and toast components into the main application

--- Frontend/ng.Frontend/src/app/app.ts
+++ Frontend/ng.Frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ModalComponent } from './shared/components/modal/modal.component';
+import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ModalComponent],
+  imports: [RouterOutlet, ModalComponent, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
