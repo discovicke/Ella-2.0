9994467fe50290be24186e2514092f4a5b13c3e9
Viktor Johansson <discovicke@gmail.com>
2026-02-10 15:08:53 +0100

    feat(administrator-layout): implement layout structure with sidebar navigation and responsive design

--- Frontend/ng.Frontend/src/app/pages/administrator/administrator.layout.ts
+++ Frontend/ng.Frontend/src/app/pages/administrator/administrator.layout.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-administrator-layout',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './administrator.layout.html',
   styleUrl: './administrator.layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
