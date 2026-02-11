271f65aeee10d517979223049076ded02483bdc9
aponten <andyponten@gmail.com>
2026-02-11 12:03:00 +0100

    feat: introduce header component across all pages

--- Frontend/ng.Frontend/src/app/pages/educator/educator.layout.ts
+++ Frontend/ng.Frontend/src/app/pages/educator/educator.layout.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-educator-layout',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './educator.layout.html',
   styleUrl: './educator.layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- Frontend/ng.Frontend/src/app/pages/student/student.layout.ts
+++ Frontend/ng.Frontend/src/app/pages/student/student.layout.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { CommonModule } from '@angular/common';
 import { signal } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 
 @Component({
   selector: 'app-student-layout',
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, HeaderComponent],
   templateUrl: './student.layout.html',
   styleUrl: './student.layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
