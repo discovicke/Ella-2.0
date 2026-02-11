73a610ad8f917d23a666ed581412c8eaf240adf2
aponten <andyponten@gmail.com>
2026-02-11 11:50:12 +0100

    Add header component

--- Frontend/ng.Frontend/src/app/pages/administrator/administrator.layout.ts
+++ Frontend/ng.Frontend/src/app/pages/administrator/administrator.layout.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-administrator-layout',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './administrator.layout.html',
   styleUrl: './administrator.layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- Frontend/ng.Frontend/src/app/shared/components/header/header.component.ts
+++ Frontend/ng.Frontend/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-
+async onClick(){
+  console.log("Du blev utloggad")
+}
 }
