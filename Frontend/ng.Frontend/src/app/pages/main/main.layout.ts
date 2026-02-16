c80cd28e2fbffb204a5e36f387f33fb23d246810
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2026-02-16 20:16:54 +0100

    refactor(layout): enhance mobile header and sidebar layout for improved usability

--- Frontend/ng.Frontend/src/app/pages/main/main.layout.ts
+++ Frontend/ng.Frontend/src/app/pages/main/main.layout.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { HeaderComponent } from '../../shared/components/header/header.component';
 import { LayoutService } from '../../shared/services/layout.service';
 import { CommonModule } from '@angular/common';
 import { SessionService } from '../../core/session.service';
@@ -8,7 +7,7 @@ import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './main.layout.html',
   styleUrl: './main.layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
