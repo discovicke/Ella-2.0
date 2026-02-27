9a5cfd5efe5caccc8940c4848dbf3d9bbe3a649c
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2026-02-27 13:58:33 +0100

    style: format scrollbar styles and import statements for consistency

--- Frontend/ng.Frontend/src/app/shared/services/booking.service.ts
+++ Frontend/ng.Frontend/src/app/shared/services/booking.service.ts
@@ -1,7 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CreateBookingDto, BookingDetailedReadModel, BookingStatus, PagedResultOfBookingDetailedReadModel } from '../../models/models';
+import {
+  CreateBookingDto,
+  BookingDetailedReadModel,
+  BookingStatus,
+  PagedResultOfBookingDetailedReadModel,
+} from '../../models/models';
 
 export interface BookingFilterParams {
   userId?: number;
