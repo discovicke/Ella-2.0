044b974a3aa4830a2882610a674ef1d4a51d714a
disco <discovicke@gmail.com>
2026-03-09 12:23:05 +0100

    feat: implement parser interface and student import DTO

---
+++ Backend/app/API/Endpoints/ImportEndpoint.cs
@@ -0,0 +1,12 @@
+using Backend.app.Core.Models.Entities;
+using Backend.app.Infrastructure.Parser;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Backend.app.API.Endpoints;
+
+public class ImportEndpoint
+{
+    
+
+}
---
+++ Backend/app/Core/Models/DTO/StudentImportDto.cs
@@ -0,0 +1,10 @@
+namespace Backend.app.Core.Models.DTO;
+
+public class StudentImportDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string City { get; set; }
+    public string ClassName { get; set; }
+}
